d9b9eea0ebf2608ca5b09aae097a7ba88e923d45
Martin Osterwalder <swiss16@users.noreply.github.com>
2014-11-28 11:17:03 +0100

    Ladekunden Funktion

---
+++ DA_Buchhaltung/DA_Buchhaltung/model/Kunde.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA_Buchhaltung.model
+{
+    public class Kunde
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Vorname { get; set; }
+        public string Adresse { get; set; }
+        public int PLZ { get; set; }
+        public string Wohnort { get; set; }
+        public string TelPrivat { get; set; }
+        public string TelMobile { get; set; }
+        public string Email { get; set; }
+        public bool Reminder { get; set; }
+        public DateTime ErfDatum { get; set; }
+    }
+}
---
+++ DA_Buchhaltung/DA_Buchhaltung/model/Model.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DA_Buchhaltung.wrapper;
+
+namespace DA_Buchhaltung.model
+{
+    public class Model
+    {
+        //Private Properties
+        DBWrapper dbWrapper = new DBWrapper();
+        private List<Kunde> kundenListe;
+
+
+        public List<Kunde> LadeKunde()
+        {
+            kundenListe = dbWrapper.LadeKunden();
+
+
+            return kundenListe;
+        } 
+
+
+
+
+    }
+}
