17ed581fc558a27799bf5e9332d3d0ce385a0a82
Martin Osterwalder <swiss16@users.noreply.github.com>
2014-12-31 16:14:58 +0100

    erstellen des Grundgerüstes des Hauptfensters. Mit Navigation Links und Buttons dazu, aber ohne Commands

--- DA_Buchhaltung/DA_Buchhaltung/viewModel/MainViewModel.cs
+++ DA_Buchhaltung/DA_Buchhaltung/viewModel/MainViewModel.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using DA_Buchhaltung.common.commands;
 using DA_Buchhaltung.model;
 
@@ -15,7 +17,21 @@ namespace DA_Buchhaltung.viewModel
         Model model = new Model();
 
         //Properties
-        private Kunde _aktuellerKunde;
+        private string _currentDate = string.Format("Datum: {0}", DateTime.Now.ToShortDateString());
+        public string CurrentDate
+        {
+            get { return _currentDate; }
+            set
+            {
+                if (CurrentDate != _currentDate)
+                {
+                    _currentDate = value;
+                    OnPropertyChanged("CurrentDate");
+                }
+            }
+        }
+
+        private Kunde _aktuellerKunde = new Kunde();
         public Kunde AktuellerKunde
         {
             get { return _aktuellerKunde; }
@@ -28,6 +44,7 @@ namespace DA_Buchhaltung.viewModel
                 }
             }
         }
+
         private bool _kundenListeIstNichtLeer;
         public bool KundenListeIstNichtLeer
         {
@@ -56,7 +73,7 @@ namespace DA_Buchhaltung.viewModel
             }
         }
 
-        private Erfolgsrechnung _aktuelleErfolgsrechnung;
+        private Erfolgsrechnung _aktuelleErfolgsrechnung = new Erfolgsrechnung();
         public Erfolgsrechnung AktuelleErfolgsrechnung
         {
             get { return _aktuelleErfolgsrechnung; }
@@ -98,6 +115,11 @@ namespace DA_Buchhaltung.viewModel
         {
             get { return _ladeKundenCommand ?? (_ladeKundenCommand = new RelayCommand<bool>(LadeKunden)); }
         }
+        private RelayCommand<string> _beendenCommand;
+        public RelayCommand<string> BeendenCommand
+        {
+            get { return _beendenCommand ?? (_beendenCommand = new RelayCommand<string>(Beenden)); }
+        }
 
 
 
@@ -123,15 +145,20 @@ namespace DA_Buchhaltung.viewModel
             SelectedKundenIndex = 0;
             KundenListeIstNichtLeer = true;
 
-            Erfolgsrechnung erf = model.ErstelleErfolgsrechnung(2014);
-            model.SpeichereErfolgsrechnung(erf);
 
+            AktuelleErfolgsrechnung = model.ErstelleErfolgsrechnung(2014);
+            model.SpeichereErfolgsrechnung(AktuelleErfolgsrechnung);
+
+        }
+
+        private void Beenden(string x)
+        {
+            Application.Current.Shutdown();
         }
 
         public MainViewModel()
         {
-            AktuellerKunde = new Kunde();
-            AktuelleErfolgsrechnung = new Erfolgsrechnung();
+            
         }
 
 
