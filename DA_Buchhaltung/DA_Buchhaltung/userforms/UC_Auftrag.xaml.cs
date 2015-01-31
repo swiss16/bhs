6ce1569a67a4d168840a5ac7059bc4b8cccc97d2
Martin Osterwalder <swiss16@users.noreply.github.com>
2015-01-31 11:01:04 +0100

    beginn Auftragview

---
+++ DA_Buchhaltung/DA_Buchhaltung/userforms/UC_Auftrag.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DA_Buchhaltung.viewModel;
+
+namespace DA_Buchhaltung.userforms
+{
+    /// <summary>
+    /// Interaktionslogik für UC_Auftrag.xaml
+    /// </summary>
+    public partial class UC_Auftrag : UserControl
+    {
+
+        private readonly AuftragViewModel _viewModel = new AuftragViewModel();
+
+        public AuftragViewModel ViewModel
+        {
+            get { return _viewModel; }
+            
+        }
+
+        public UC_Auftrag()
+        {
+            InitializeComponent();
+        }
+
+        private void Validation_Error(object sender, ValidationErrorEventArgs e)
+        {
+            if (e.Action == ValidationErrorEventAction.Added) AuftragViewModel.Errors += 1;
+            if (e.Action == ValidationErrorEventAction.Removed) AuftragViewModel.Errors -= 1;
+            
+        }
+        
+    }
+}
