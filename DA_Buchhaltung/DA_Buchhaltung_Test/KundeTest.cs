a19fee793cb4300506ebadc9bf89beb8e55fe0a2
Martin Osterwalder <swiss16@users.noreply.github.com>
2015-02-06 11:38:20 +0100

    Testprojekt erstellt, Unittests 1-3 (Kundentests) erstellt und alle sind ok

---
+++ DA_Buchhaltung/DA_Buchhaltung_Test/KundeTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DA_Buchhaltung.data;
+using DA_Buchhaltung.model;
+using DA_Buchhaltung.viewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DA_Buchhaltung_Test
+{
+    [TestClass]
+    public class KundeTest
+    {
+        [TestMethod]
+        public void KundeErfassenTest_KorrektesErfassen()
+        {
+            //Arrange
+            Model model = new Model();
+            Kunde sollKunde = new Kunde
+            {
+                Adresse = "Teststrasse",
+                Email = "Test@test.ch",
+                Name = "Tester",
+                Vorname = "Test",
+                PLZ = 8888,
+                Wohnort = "Teststadt"
+            };
+
+            //Act
+            int id = model.SpeichereKunde(sollKunde);
+            List<Kunde> klist = model.LadeKunden("");
+            Kunde istKunde = klist.First(i => i.ID == id);
+
+
+            //Assert
+            Assert.IsNotNull(istKunde);
+            Assert.IsTrue(sollKunde.Adresse == istKunde.Adresse);
+        }
+
+        [TestMethod]
+        public void KundeAendernTest_KorrektesSpeichern()
+        {
+            //Arrange
+            Model model = new Model();
+            List<Kunde> klist = model.LadeKunden("");
+            Kunde istKunde = klist.First(i => i.Adresse == "Teststrasse");
+            istKunde.Adresse = "Teststrasse2";
+            Kunde sollKunde = new Kunde
+            {
+                Adresse = "Teststrasse2",
+                Email = "Test@test.ch",
+                Name = "Tester",
+                Vorname = "Test",
+                PLZ = 8888,
+                Wohnort = "Teststadt"
+            };
+
+            //Act
+            int id = model.SpeichereKunde(istKunde);
+            klist = model.LadeKunden("");
+            istKunde = klist.First(i => i.ID == id);
+
+
+            //Assert
+            Assert.IsNotNull(istKunde);
+            Assert.IsTrue(sollKunde.Adresse == istKunde.Adresse);
+        }
+
+        [TestMethod]
+        public void KundeLoeschenTest_KorrektesLoeschen()
+        {
+            //Arrange
+            Model model = new Model();
+            List<Kunde> klist = model.LadeKunden("");
+            Kunde istKunde = klist.First(i => i.Adresse == "Teststrasse2");
+            
+
+            //Act
+            bool successfull = model.LoescheKunde(istKunde);
+            klist = model.LadeKunden("");
+            bool nochVorhanden = klist.Any(i => i.Adresse == "Teststrasse2");
+
+
+            //Assert
+            Assert.IsTrue(successfull);
+            Assert.IsFalse(nochVorhanden);
+        }
+    }
+}
---
+++ DA_Buchhaltung/DA_Buchhaltung_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über folgende 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("DA_Buchhaltung_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DA_Buchhaltung_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Wenn ComVisible auf "false" festgelegt wird, sind die Typen innerhalb dieser Assembly 
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("39669e3a-ad39-488c-9b82-4be8ebaab725")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// durch Einsatz von '*', wie in nachfolgendem Beispiel:
+// [Assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
